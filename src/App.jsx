38d3797b44c0adc68897f061036633fc4cc6db60
farial-robama <farialrobama15@gmail.com>
2025-10-01 22:25:40 +0200

    Add Navbar & Logo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-
-
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <>
+    <header className='w-11/12 mx-auto py-3'>
+      <Navbar></Navbar>
+ 
+    </header>
+    
       
     </>
   )
